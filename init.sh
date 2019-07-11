22965aafee751ddaa74a1c9d08f975127e4d820a
yassu <yassu0320.dev@gmail.com>
2019-07-11 12:33:38 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# install git-completion.bash
+if [ -e "~/.git-completion.bash" ]; then
+    cmd="curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash"
+    `$cmd`
+fi
+
 dot_files=`find . -name "_*" -maxdepth 1`
 for dot_file in $dot_files;
 do
