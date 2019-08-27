d634af5bcfa42005bcb9b04f6e7415bb1513fa21
yassu <mathyassu@gmail.com>
2019-08-28 03:12:20 +0900

    🎨 [bashrc] Refactoring

--- init.sh
+++ init.sh
@@ -30,7 +30,7 @@ dot_files=`find . -name "_*" -maxdepth 1`
 for dot_file in $dot_files;
 do
     dot_file=`basename $dot_file`
-    cmd="ln -sf  dotfiles/$dot_file `(cd ~ && pwd)`/.${dot_file:1}"
+    cmd="ln -sf  dotfiles/$dot_file ${HOME}/.${dot_file:1}"
     echo $cmd
     `$cmd`
 done
